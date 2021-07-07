5970b0d3269f3b1df53fc5eb6d993f47e0d30448
maor6 <maor.ovadia6@gmail.com>
2021-07-07 16:11:33 +0300

    Connect to kafka cloud service

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require("socket.io")(server)
+const port = 3000
+
+//------------ kafka------------
+const kafka = require('./kafkaController/kafkaProduce');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//------------
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', (req, res) => res.send("<a href='/send'>Send</a> <br/><a href=''>View</a>"));
+app.get('/send', (req, res) => res.render('sender'));
+
+
+
+//------------ Socket.io ----------------
+io.on("connection", (socket) => {
+    console.log("new user connected");
+    socket.on("totalWaitingCalls", (msg) => { console.log(msg.totalWaiting) });
+    socket.on("callDetails", (msg) => { console.log(msg);kafka.publish(msg) });
+});
+
+
+//------------------- kafka -----------
+/* Kafka Producer Configuration */
+
+//
+//const client1 = new kafka.KafkaClient({kafkaHost: "localhost:9092"});
+
+
+
+
+
+
+//------------------------------------
+
+
+server.listen(port, () => console.log(`Ariel app listening at http://localhost:${port}`));
---
+++ kafkaController/kafkaProduce.js
@@ -0,0 +1,34 @@
+// https://www.cloudkarafka.com/ הפעלת קפקא במסגרת ספק זה
+
+const uuid = require("uuid");
+const Kafka = require("node-rdkafka");
+
+const kafkaConf = {
+  "group.id": "cloudkarafka-example",
+  "metadata.broker.list": "dory-01.srvs.cloudkafka.com:9094,dory-02.srvs.cloudkafka.com:9094,dory-03.srvs.cloudkafka.com:9094".split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": "ozdzk6dk",
+  "sasl.password": "urnoiE00jpgXLkaOvZNxeetu-21X1SZb",
+  "debug": "generic,broker,security"
+};
+
+
+const prefix = "ozdzk6dk-";
+const topic = `${prefix}cars`;
+const producer = new Kafka.Producer(kafkaConf);
+
+const genMessage = m => new Buffer.alloc(m.length,m);
+
+producer.on("ready", function(arg) {
+  console.log(`producer is ready.`);
+});
+producer.connect();
+
+module.exports.publish= function(msg)  // export a function
+{   
+  m=JSON.stringify(msg);  // turn the msg to string
+  producer.produce(topic, -1, genMessage(m), uuid.v4());  
+  //producer.disconnect();   
+}
