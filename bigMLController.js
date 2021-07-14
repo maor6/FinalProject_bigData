1b3b7874f86280644b9ebcb4a33bd5203cd193f0
maor6 <maor.ovadia6@gmail.com>
2021-07-14 16:36:37 +0300

    Add function that create model

--- bigMLController.js
+++ bigMLController.js
@@ -2,9 +2,16 @@ const bigml = require('bigml');
 const connection = new bigml.BigML('MAOR6',
     '38ac1c5989ac81437de09506c90dd86863e30a00');
 
+
+const createCSV = require('./csvWriter');
+
+var localModel;
+
 const source = new bigml.Source();
+
 const bigML = {
-    createModel: function () {
+    createModel: async function () {
+        await (createCSV.create)();
         source.create('./files/events.csv', function(error, sourceInfo) {
             if (!error && sourceInfo) {
                 const dataset = new bigml.Dataset();
@@ -14,28 +21,40 @@ const bigML = {
                         model.create(datasetInfo, function (error, modelInfo) {
                             if (!error && modelInfo) {
                                 console.log(modelInfo.resource);
-                                let localModel = new bigml.LocalModel(modelInfo.resource);
-                                // return localModel;
-                                const prediction = new bigml.Prediction();
-                                prediction.create(modelInfo, {'eventType': "exit road"}, function (error, predictionInfo) {
-                                    console.log(prediction.resource);
-                                    const localModel = new bigml.LocalModel(prediction.resource);
-                                    localModel.predict({'carType': "truck", 'eventType': "exit road"}, function(error, prediction) {
-                                        console.log("the prediction is: " + prediction.prediction);
-                                    });
-                                });
+                                localModel = new bigml.LocalModel(modelInfo.resource);
                             }
                         });
                     }
                 });
             }
         });
-    }
-}
+    },
 
-bigML.createModel();
+    predict: function (event) {
+        if (localModel) {
+            const prediction = new bigml.Prediction();
+            prediction.create(localModel, {'eventType': "exit road"}, function (error, predictionInfo) {
+                if (!error && predictionInfo) {
+                    console.log(prediction.resource);
+                    const localModel = new bigml.LocalModel(prediction.resource);
+                    localModel.predict({'_id': event._id, 'carType': event.carType, 'eventType': "exit road"}, function(error, prediction) {
+                        console.log("the prediction is: " + prediction.prediction);
+                    });
+                }
+            });
+        }
+        else {
+            // TODO send to client that he need first to train
+        }
+    }
+};
+
+
 // const localModel = new bigml.LocalModel(prediction.resource);
 // localModel.predict({'carType': "truck", 'eventType': "exit section"},
 //     function(error, prediction) {
 //         console.log("the prediction is: " + prediction.prediction);
 //     });
+
+
+module.exports = bigML
