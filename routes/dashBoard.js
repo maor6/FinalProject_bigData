620540766b9d7b102ae763d7dae4d1a253a1b19e
maor6 <maor.ovadia6@gmail.com>
2021-07-16 01:11:09 +0300

    Add listen to updates from redis

--- routes/dashBoard.js
+++ routes/dashBoard.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 //-----------redis--------------
+const redis = require('./../model/RedisForArielReciver');
+
 
 
 router.use(function(req, res, next) {
@@ -12,7 +14,7 @@ router.use(function(req, res, next) {
 router.route('/').get(((req, res) => {
     const cardData = {
         id: "totalSum",
-        title: "אריאל",
+        title: "Number of cars in the road",
         totalSum: 5,
         percent: 0.8,
         icon: "work"
@@ -20,5 +22,14 @@ router.route('/').get(((req, res) => {
     const cards = ["Borrowed", "Annual Profit", "Lead Conversion", "Average Income",];
     res.render("./pages/index",{card:cardData});
 }));
+
+
+redis.redisC.on("message", function (channel, msg) {
+    io.emit('new data', msg);
+    // redis.NumOfCars(send);
+    // io.emit('new data', redis.NumOfCars());
+    // TODO with WS render the page to update the number of cars
+});
+
 
 module.exports = router;
